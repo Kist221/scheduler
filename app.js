6a6777fff75523c2369b93e60d614b7682443308
Kist221 <Kist221@gmail.com>
2018-05-25 17:00:45 -0700

    es6 includes

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const now = new Date();
-// 0 - 6 (sun - sat)
-const day = now.getDay();
-// military time (24 hours / 0 - 23)
-const hour = now.getHours();
-const min = now.getMinutes();
+// day = 0-6 (sun-sat)
+// hour = (24 hours / 0-23)
+const [day, hour, min] = [now.getDay(), now.getHours(), now.getMinutes()];
 
+// TODO: function for each time slot check...
+console.log(now);
+console.log(day, hour, min);
+
+// determines time for weekday and proceeds
+const weekTime = (h, c) => {
+    console.log(h);
+
+};
+weekTime(hour);
+
+// determines day and proceeds
 const winning = (d, h, m) => {
-    console.log(d, h, m);
     // if mon-fri
     // && because || returns 0 as true
     if (d > 0 && d < 6) {
-        console.log(true)
-    } else {
-        console.log(false);
+        console.log('weekday');
+    }
+    // if saturday 
+    else if (d === 6) {
+        console.log('saturday');
+    }
+    // if sunday
+    else if (d === 0) {
+        console.log('sunday');
     }
 };
 
