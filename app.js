06835dcd06e5c62ceaa7d15c84d1db2eb26c257c
Kist221 <Kist221@gmail.com>
2018-05-29 11:30:07 -0700

    updated schedule && changed destructured vars to objects

--- app.js
+++ app.js
@@ -1,37 +1,59 @@
 const now = new Date();
 // day = 0-6 (sun-sat)
 // hour = (24 hours / 0-23)
-const [day, hour, min] = [now.getDay(), now.getHours(), now.getMinutes()];
-
-// TODO: function for each time slot check...
+const current = {
+    day: now.getDay(),
+    hour: now.getHours(),
+    min: now.getMinutes()
+}
+// BIZ HOURS - closed weekdays @ 6 && sat @ 4?
+const closed = {
+    weekday: 18,
+    saturday: 16
+}
 console.log(now);
-console.log(day, hour, min);
+console.log(current.day, current.hour, current.min);
 
-// determines time for weekday and proceeds
-const weekTime = (h, c) => {
-    console.log(h);
-
+// check if closed on weekday
+const weekTime = (h, closed) => {
+    // if before 6am && past 6pm
+    if (h < 6 || h > closed) {
+        console.log('closed');
+        // TODO: display message on page
+    } else {
+        // DO NOTHING
+        console.log('open', h, closed);
+    }
 };
-weekTime(hour);
 
-// determines day and proceeds
+// check if closed on Saturday
+const satTime = (h, closed) => {
+    // if before 6am && past 6pm
+    if (h < 6 || h > closed) {
+        console.log('closed');
+        // TODO: display message on page
+    } else {
+        // DO NOTHING
+        console.log('open', h, closed);
+    }
+};
+
+// determines day - input day, hour, min
 const winning = (d, h, m) => {
-    // if mon-fri
-    // && because || returns 0 as true
+    // mon-fri
     if (d > 0 && d < 6) {
         console.log('weekday');
+        weekTime(h, closed.weekday);
     }
-    // if saturday 
+    // saturday 
     else if (d === 6) {
         console.log('saturday');
+        satTime(h, closed.saturday);
     }
-    // if sunday
+    // sunday
     else if (d === 0) {
         console.log('sunday');
     }
 };
 
-
-
-
-winning(day, hour, min);
+winning(current.day, current.hour, current.min);
